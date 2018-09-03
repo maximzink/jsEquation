ad9420a5af659ff1eecd0ce2e846ed1b7650633b
MaximZinchuk <maximzink@bigmir.net>
2018-09-03 12:48:00 +0000

    solution

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 function quadric(a,b,c){
     
-    const descriminant = Math.pov(b,2)-(4*a*c);
+    const descriminant = (Math.pow(b,2))-(4*a*c);
     if (descriminant<0){
         return "the equation has no solution";
     }
@@ -10,11 +10,11 @@ function quadric(a,b,c){
         return "It is linear equation";
         
     } else if(descriminant>0){
-        const x1 = (-b - Math.sqrt(descriminant))/ (2*a);
-        const x2 = (-b + Math.sqrt(descriminant))/ (2*a);
-        
-        const solution = "x1= " + "<br>" + x1 + "x2= " +x2;
-        return solution;
+    const x1 = (-b - Math.sqrt(descriminant))/ (2*a);
+    const x2 = (-b + Math.sqrt(descriminant))/ (2*a);
+    
+    const solution = "x1= " + x1 + "\nx2= " +x2;
+    return solution;
     }
 }
 
@@ -25,3 +25,4 @@ const c = parseFloat(prompt("Enter third number"));
 const solution = quadric(a,b,c);
 
 alert(solution);
+
---
+++ js/index1.js
@@ -0,0 +1,8 @@
+
+var a = 55;
+var b = 2;
+var c=2;
+
+    const descriminant = (Math.pow(b,2))-(4*a*c);
+    
+    alert (descriminant);
